43071a8c547118453ef959ff17f8a5d79cfaada0
Imran Malik <imran.malik@stem.com>
2017-04-16 19:48:38 -0700

    remove old db migrate

--- db/migrate/20170411025140_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-      t.column :first_name, :string
-      t.column :last_name, :string
-      t.timestamps null: false
-    end
-  end
-end
